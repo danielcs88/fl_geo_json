e6395377ba62328453cca7d7c9c295b1c389413b
Daniel Cárdenas <danielcs88@users.noreply.github.com>
2020-08-01 03:58:52 -0400

    Pythonic approach: 20 mins, Last approach: 20 hours

---
+++ json_slice.py
@@ -0,0 +1,37 @@
+# %% [markdown]
+# # Florida JSON
+
+# %%
+import json
+from urllib.request import urlopen
+
+with urlopen(
+    "https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json"
+) as response:
+    counties = json.load(response)
+
+# %%
+print(type(counties))
+
+# %%
+type(counties.get("features"))
+
+# %%
+import pandas as pd
+
+test = counties.get("features")
+
+# %%
+florida = []
+
+# %%
+for entry in test:
+    if entry.get("properties").get("STATE") == '12':
+        florida.append(entry)
+
+# %%
+fl_json = dict(type="FeatureCollection", features=florida)
+
+# %%
+with open('fl_json.json', 'w') as fp:
+    json.dump(fl_json, fp)
